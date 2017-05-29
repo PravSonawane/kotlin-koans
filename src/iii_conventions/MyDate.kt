8c356c9a529f4d85bf27c34187fd97eb2e075edd
PravinSonawane <june.pravin@gmail.com>
2017-05-29 13:21:41 -0700

    Solved 28

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -20,9 +20,28 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate) {
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return DateIterator(start,endInclusive)
+    }
+
     operator fun contains(date: MyDate): Boolean {
         return date > start && date < endInclusive
+    }
+
+}
+
+class DateIterator(start: MyDate, endInclusive: MyDate) : Iterator<MyDate> {
+    var current = start
+    val last = endInclusive
+    override fun hasNext(): Boolean {
+        return current <= last
+    }
+
+    override fun next(): MyDate {
+        val result = current
+        current = current.addTimeIntervals(TimeInterval.DAY,1)
+        return result
     }
 
 }
